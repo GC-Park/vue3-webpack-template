a0b799e64225f050a93dcb94c5bf939bfdf5f524
GC-Park <kuyt3122@naver.com>
2022-03-05 00:43:10 +0900

    Vue3 Template

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env : {
+    browser: true,
+    node: true
+  },
+  extends: [
+    // vue
+    // 'plugin:vue/vue3-essential', //Lv1
+    'plugin:vue/vue3-strongly-recommended', // Lv2
+    // 'plugin:vue/vue3-recommended', // Lv3
+    // js
+    'eslint:recommended'
+  ],
+  parser: 'vue-eslint-parser',
+  rules: {
+    "vue/html-closing-bracket-newline": ["error", {
+      "singleline": "never",
+      "multiline": "never"
+    }],
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "never",
+        "normal": "never",
+        "component": "always"
+      },
+      "svg": "always",
+      "math": "always"
+    }]
+  }
+}
